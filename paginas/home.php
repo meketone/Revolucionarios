03956bd94f587cd6b854073df719b44a6ed91c4d
meketone <meketone17@gmail.com>
2024-05-05 17:01:46 -0600

    Ya quedo la pagina principal

--- paginas/home.php
+++ paginas/home.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Los Revolucionarios</title>
     <link rel="stylesheet" href="..\css\home.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"> -->
 
 
 
@@ -29,19 +29,44 @@
             </ul>
         </nav>
     </header> -->
+    <div class="wave">
+        <label for="">Encuentra</label>
+    </div>
+    <div class="wave1">
+        <label class="label">tu antojo</label>
+    </div>
+    <div class="wave2">
+        <label class="label">Favorito</label>
+        <a href="#menu" class="button-main-content">IR AL MENÚ</a>
+        <p style="letter-spacing: .1rem;">Consulta nuestras platillos ahora mismo</p>
+        <div class="swiper-pagination"></div>
+    </div>
+    
     <div class="swiper-container">
+
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
             <!-- Slides -->
-            <div class="swiper-slide"><img src="../imagenes/editadas/_DSC0023.jpg" alt="Imagen 1"></div>
-            <div class="swiper-slide"><img src="../imagenes/editadas/_DSC0047.jpg" alt="Imagen 2"></div>
-            <div class="swiper-slide"><img src="../imagenes/editadas/_DSC0059.jpg" alt="Imagen 3"></div>
+            <div class="swiper-slide">
+                    <img src="../imagenes/editadas/_DSC0023.jpg" alt="Imagen 1">
+            </div>
+            <div class="swiper-slide">
+                    <img src="../imagenes/editadas/_DSC0047.jpg" alt="Imagen 2">
+            </div>
+            <div class="swiper-slide">
+                    <img src="../imagenes/editadas/_DSC0059.jpg" alt="Imagen 3">
+            </div>
         </div>
+
         <!-- If we need pagination -->
-        <div class="swiper-pagination"></div>
+        <!-- <div class="swiper-pagination"></div> -->
         <!-- Button placed over the carousel -->
-        <a href="#menu" class="button main-content">Ir al Menú</a>
     </div>
+
+
+
+
+
     <?php require('..\layouts\footer.php') ?>
     <!-- <footer>
         <img src="path/to/logo.png" alt="Logo de la compañía" class="logo">
@@ -51,6 +76,16 @@
 
     <!-- <script src="js\scriptmenu.js"></script> -->
     <script src="..\js\home.js"></script>
+    <script>
+        const waveElements = document.querySelectorAll('.wave label');
+        waveElements.forEach(label => {
+            label.innerHTML = label.textContent.split('').map((text, wave) =>
+                `<span style="transition-delay: ${wave * 25}ms">${text}</span>`
+            ).join('');
+        });
+    </script>
+
+
     </body>
 
 </html>
