129132eb930684c1581dc0b41d4198c394c36df7
Stibuwu <stevesaul7@gmail.com>
2024-05-05 00:34:55 -0600

    Seccion de nosotros en su 90%

--- paginas/noso.php
+++ paginas/noso.php
@@ -28,88 +28,72 @@
     </nav> --> 
 
     <main>
-        <section id="TituloA"><br><br><br><br>
-            <h1>Acerca de RUOK?</h1>
+        <section id="TituloA"><br>
+            <h1>Acerca de nuestro Menú</h1>
         </section>
         <section id="nuestra-historia">
             <div class="container">
                 <div class="imagen">
-                    <img src="img/RUOKiso.jpeg" alt="Nuestra Histria">
+                    <img src="..\imagenes\editadas\Blancas\_DSC0135.jpg" alt="Nuestra Menú" width= "500px">
                 </div>
                 <div class="texto">
-                    <h1 class="sangria">Nuestra Historia </h1>
                     <p class="sangria">
-                        RUOK? 
-                    </p>
-                    <p class="sangria">
-                        Desarrollo web el cual busca la integración de la inteligencia artificial para la ayuda psicológica en las personas, 
-                        además de un portal de ayuda para todos aquellos que deseen expulsar sus frustraciones, tristezas y preocupaciones tanto 
-                        con una IA o un corazón en la misma situación que tú. Además de que se contará con varios contactos y centros de ayuda si 
-                        así lo deseas.
-                    </p>
-                    <p class="sangria">
-                        Hola mi nombre es Lucy seré tu compañera en este hermoso mundo de ensueño donde podrás comunicarte con otros corazones agrietados.
-                        No importa que tan pequeño o grande sea tu problema, cada quien esta pasando por algo difícil. En este hermoso sueño tendrás 
-                        la tarea de ayudar a personas que se encuentran en tu misma situación, pues al final de cuentas todos somos tu.
+                        Nuestro menú está cuidadosamente elaborado 
+                        para <b> ofrecer una amplia variedad de platillos tradicionales mexicanos</b>, 
+                        desde tacos y enchiladas hasta pozole y mole. Cada receta se prepara con pasión y atención 
+                        al detalle, <b> para que cada visita sea una experiencia inolvidable llena de sabores aiténticos
+                        y momentos memorables. </b>
                     </p>
                 </div>
             </div>
         </section>
         <section id="TituloV">
-            <h1> Visión </h1>
+            <h1> Acerca de nuestra comida </h1>
         </section>
         <section id="vision">
             <div class="container-vision">
-                <div class="boxe">
-                    <h1>Terapeuta virtual personalizado:</h1>
-                    <p>En RUOK? ofecemos sesiones de terapia personalizadas utilizando algoritmos de IA que analizan los patrones de comportamiento y las respuestas del usuario. A través de conversaciones simuladas, la IA podra proporcionar orientación, ejercicios de relajación y técnicas de afrontamiento adaptadas a las necesidades específicas del individuo.</p>
+                <div class="textoV">
+                    <p class="sangria">
+                        En Los Revolucionarios, nos enorgullece ofrecerte una experiencia culinaria, auténtica y fresca todos los días. 
+                    </p><br>
+                    <p class ="sangria">
+                        Nuestro compromiso es simple pero poderoso: cada plato se elabora al día con los ingredientes más frescos y de 
+                        la más alta calidad, asegurando que disfrutes de sabores genuinos en cada bocado.
+                    </p><br>
+                    <p class="sangria">
+                        Nos esforzamos por brindarte la máxima disponibilidad posible, para que siempre encuentres un lugar en nuestra mesa cuando 
+                        el antojo de auténtica comida mexicana te llame.
+                    </p><br>
+                    <p class="sangria">
+                        En <b>Los Revolucionarios</b> siempre te recibiremos con una sonrisa y platos que te transportarán a los sabores tradicionales de México.
+                    </p> 
                 </div>
-                <div class="boxe">
-                    <h1>Seguimiento emocional y alertas tempranas:</h1>
-                    <p>Lucy podra utilizar análisis de datos y procesamiento de lenguaje natural para monitorear las interacciones del usuario en redes sociales, mensajes de texto y correos electrónicos. Al detectar cambios significativos en el tono o el contenido de las comunicaciones, Lucy podría enviar alertas tempranas y sugerir recursos de apoyo o intervención profesional si es necesario.</p>
-                </div>
-                <div class="boxe">
-                    <h1>Entrenador de habilidades sociales:</h1>
-                    <p>Lucy podra ofrecer simulaciones de situaciones sociales y proporcionar retroalimentación sobre las respuestas del usuario, ayudándoles a mejorar sus habilidades de comunicación, empatía y resolución de conflictos. Además, Lucy podría ofrecer consejos personalizados para enfrentar situaciones sociales difíciles, como hablar en público o establecer límites saludables en las relaciones interpersonales.</p>
-                </div>
-                <div class="boxe">
-                    <h1>Diario emocional inteligente:</h1>
-                    <p>Lucy podra servir como un diario emocional interactivo, donde los usuarios registran sus pensamientos, sentimientos y experiencias diarias. Utilizando algoritmos, Lucy podría identificar patrones emocionales, factores desencadenantes y tendencias a lo largo del tiempo. Con esta información, Lucy podría ofrecer insights y recomendaciones personalizadas para mejorar el bienestar emocional y la salud mental del usuario.</p>
+                <div class="grid-img">
+                    <div class="img-grid"> <img src="..\imagenes\editadas\_DSC0068.jpg" alt="Imagen 1"> </div>
+                    <div class="img-grid"> <img src="..\imagenes\editadas\_DSC0057.jpg" alt="Imagen 2"> </div>
+                    <div class="img-grid"> <img src="..\imagenes\editadas\_DSC0042.jpg" alt="Imagen 3"> </div>
                 </div>
             </div>
+            <br>
         </section>
-        <section id="TituloPP">
-            <h1>Politica de privacidad</h1>
-        </section>
-        <section id="PoliticaP">
-            <div class="container-p">
-                <div class="texto">
-                    <h1 class="sangria">Aviso legal</h1>
-                    <p class="sangria">
-                        RUOK? es una pagina que funciona como foro de difusión y discusión sobre temas sensibles, recomendamos la discreción de los 
-                        usuarios. Si bien RUOK? cuenta con un sistema de gestión de temas, comentarios u opiniones respecto a ciertos tópicos o palabras, 
-                        el usuario tiene que hacerse responsable respecto a sus acciones y actuar en esta pagina. RUOK? no esta relacionado con actos de 
-                        odio, opiniones ofensivas o incluso el mal manejo de la pagina. RUOK? cuenta con un sistema de baneo y censura de algunas 
-                        expresiones o palabras de odio o desprecio, al aceptar los términos y condiciones da consentimiento a RUOK? el monitoreo constante 
-                        de sus conversaciones respecto a todos sus servicios. Lo que buscamos es la integridad de nuestros usuarios y deseamos con fuerza 
-                        una convivencia sana con todos nuestros usuarios. Esto aplicara en los servicios de foro , IA y canalización. RUOK? tomara medidas 
-                        legales con usted si llega a amedrentar, lastimar, manipular, extorsionar e incluso atentar contra la vida de un usuario.
-                    </p><br>
-                    <h1 class="sangria">Politica de Privacidad</h1>
-                    <p class="sangria">
-                        Da consentimiento de que RUOK? recopile su información personal para el uso con terceros, sus datos privados serán utilizados 
-                        para el entrenamiento de la inteligencia artificial, además que dichos datos también serán utilizados por entidad es 
-                        especialidades en el campo de la psicología para su análisis. Usted da consentimiento que sus datos personales y las métricas 
-                        que obtenga en ciertos puntos serán utilizados a su vez para la investigación. RUOK? se compromete a el cifrado y el excelente 
-                        manejo de dicha información, todo desde el anonimato y con la seguridad completa de datos personales así como de sus conversaciones 
-                        privadas. En RUOK? le damos el control respecto a su información personal, usted tiene el control de como puede interactuar y sobre el 
-                        como los otros usuarios interactúan con usted .Si se llega a presentar algún cambio con los términos y condiciones se le hará saber a cada 
-                        uno de los usuarios que estén registrados en la pagina. Si tiene alguna inconformidad con los terminos y condiciones puede contactarse con 
-                        nosotros en R-U-O-Kmind_Helt@gmail.com
+        <section id="Eeventos">
+            <div class="container-E">
+                <div class="texto-E">
+                    <h1 class="sangria">Explora Nuestros</h1>
+                    <h1 class="sangria">Eventos</h1>
+                    <br>
+                    <p class="sangria-E">
+                        Encuéntranos en redes sociales para estar 
                     </p>
-                </div>
-                <div class="imagen-p">
-                    <img src="img/atardecer.jpg" alt="Imagen llamativa" width="600px" height="1000px">
+                    <p class="sangria-E">
+                        al tanto de los eventos que organizamos en
+                    </p>
+                    <p class="sangria-E">
+                        nuestros locales todos los viernes, con
+                    </p>
+                    <p class="sangria-E">
+                        música jazz en vivo.  #JazzFridays
+                    </p>
                 </div>
             </div>
         </section>
